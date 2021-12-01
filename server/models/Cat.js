df5a31dba685d4d86ccaa33af1f78f8a8780de62
Derek Banister <ptownderek@yahoo.com>
2021-11-30 20:35:08 -0800

    adding base models/thoughts on properties of models

---
+++ server/models/Cat.js
@@ -0,0 +1,17 @@
+const { Schema } = require('mongoose')
+
+const catSchema = new Schema({
+    //name?
+
+    //role?
+
+    //image?
+
+    //description? 
+
+    //power/abilities or something
+
+    // Owners?
+})
+
+module.exports = catSchema
