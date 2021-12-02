3bb6b7ff63efc17756c2d5abaa5518ae2e92f3bf
Vince <starryblue7@gmail.com>
2021-12-01 20:06:14 -0800

    add Footer & Sidebar components

---
+++ client/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Page footer
+function Footer() {
+    return (
+        <footer className="col-12">
+            &copy; The D.E.V. Team, 2021
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Sidebar
+function Sidebar() {
+    return (
+        <aside>
+            Sidebar
+        </aside>
+    );
+}
+
+export default Sidebar;
