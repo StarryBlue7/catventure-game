135777ec4a86dad4214905cf043c43bede501f6a
Vince <starryblue7@gmail.com>
2021-12-01 21:45:09 -0800

    add initial header & nav styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Navigation from './Navigation';
 
+import logo from '../images/catventureLogo.png'
+
+const styles = {
+    header: {
+        backgroundColor: "orange"
+    },
+    logo: {
+        width: 500
+    }
+}
+
 // Page header
 function Header() {
     return (
-        <header className="col-12">
-            <div>Logo</div>
+        <header className="col-12 d-flex flex-column align-items-center pt-3 pb-2" style={styles.header}>
+            <img src={logo} alt="Catventure!" style={styles.logo} />
             <Navigation />
         </header>
     );
--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
+let user =  {
+    name: "User"
+}
+
 // Navbar
 function Navigation() {
     return (
-        <nav>
-            Nav buttons
+        <nav className="col-12 d-flex flex-row justify-content-between px-0">
+            <ul id="nav-buttons" className="nav d-flex flex-row justify-content-end align-items-center gap-10">
+                <li className="nav-item">Home</li>
+                <li className="nav-item">Adventure</li>
+            </ul>
+            <div id="user-box" className="d-flex flex-row justify-content-end align-items-center gap-10">
+                <p className="m-0">Welcome, {user.name}!</p>
+                <button className="btn btn-sm btn-danger">Logout</button>
+            </div>
         </nav>
     );
 }
