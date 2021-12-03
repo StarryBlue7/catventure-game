589464f119db680ffdac2fe72a2d598fedcaa7a2
Emily Dorgan <wmdorgan@gmail.com>
2021-12-02 18:07:46 -0800

    added home and tavern pages, put the userData stuff in Main

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,9 +1,43 @@
-import React from 'react';
-import Sidebar from './Sidebar'
+import React, {useEffect, useState} from 'react';
+import Auth from '../utils/auth';
+import { getMe } from '../utils/API';
+import Sidebar from './Sidebar';
 import Footer from './Footer';
+
+
 
 // Main page
 function Main() {
+
+    const [userData, setUserData] = useState({});
+
+    const userDataLength = Object.keys(userData).length;
+
+    useEffect(() => {
+        const getUserData = async () => {
+        try {
+            const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+            if (!token) {
+            return false;
+            }
+
+            const response = await getMe(token);
+
+            if (!response.ok) {
+            throw new Error('something went wrong!');
+            }
+
+            const user = await response.json();
+            setUserData(user);
+        } catch (err) {
+            console.error(err);
+        }
+        };
+
+        getUserData();
+    }, [userDataLength]);
+
     return (
         <div className="col-10 row">
             <Sidebar />
@@ -16,29 +50,3 @@ function Main() {
 }
 
 export default Main;
-
-
-// useEffect(() => {
-//     const getUserData = async () => {
-//       try {
-//         const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-//         if (!token) {
-//           return false;
-//         }
-
-//         const response = await getMe(token);
-
-//         if (!response.ok) {
-//           throw new Error('something went wrong!');
-//         }
-
-//         const user = await response.json();
-//         setUserData(user);
-//       } catch (err) {
-//         console.error(err);
-//       }
-//     };
-
-//     getUserData();
-//   }, [userDataLength]);
---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <section>
+            <h2>
+                Home Sweet Home
+            </h2>
+            <div>Go on Adventure</div>
+            <div></div>
+        </section>
+    )
+}
+
+export default Home;
