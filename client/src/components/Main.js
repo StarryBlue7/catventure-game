7dfe5ab3e8a6e807cfcd48e1af5f355e9f0f09db
Derek Banister <ptownderek@yahoo.com>
2021-12-03 13:30:55 -0800

    beginning development of aside cards

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Switch, Route } from 'react-router-dom';
 import Auth from '../utils/auth';
 import { getMe } from '../utils/API';
@@ -15,35 +15,6 @@ import Cave from './pages/Cave';
 
 // Main page
 function Main() {
-
-    // const [userData, setUserData] = useState({});
-
-    // const userDataLength = Object.keys(userData).length;
-
-    // useEffect(() => {
-    //     const getUserData = async () => {
-    //     try {
-    //         const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    //         if (!token) {
-    //         return false;
-    //         }
-
-    //         const response = await getMe(token);
-
-    //         if (!response.ok) {
-    //         throw new Error('something went wrong!');
-    //         }
-
-    //         const user = await response.json();
-    //         setUserData(user);
-    //     } catch (err) {
-    //         console.error(err);
-    //     }
-    //     };
-
-    //     getUserData();
-    // }, [userDataLength]);
 
     return (
         <div className="col-10 row">
