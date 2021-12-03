58c04475f2375c3f68993b945515e5852328df1e
Emily Dorgan <wmdorgan@gmail.com>
2021-12-02 22:35:25 -0800

    finished the cat card with the exception of the modal part and API calls

--- client/src/components/gameUI/CatCard.js
+++ client/src/components/gameUI/CatCard.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState} from 'react';
+import { Button } from 'react-bootstrap';
 
 // replace these with the proper imports
 const warriorImage = '';
@@ -6,7 +7,17 @@ const rangerImage = '';
 const mageImage = '';
 
 function CatCard(props) {
+    // For this card to work, I need the following:
+    // props.cat.name
+    // props.cat.class
+    // props.cat.maxHP
+    // props.cat.level
+    // props.cat.power
+    // props.isTavern (true or false)
 
+    // I also need the addCat & removeCat function
+
+    const [namingModal, setNamingModal] = useState(false);
 
     function getClassName(gameClass){
         if(gameClass === 1){
@@ -29,12 +40,23 @@ function CatCard(props) {
     }
 
     return (
+        <>
             <div className="cat-card">
                 {props.cat.name ? (<p>{props.cat.name}</p>) : (<></>)}
                 <img src={getClassImg(props.cat.class)} alt="animated cat" />
                 <div className="hp-bar"><div></div></div>
                 <p>{getClassName(props.cat.class)}</p>
+                <p>HP: {props.cat.maxHP}/{props.cat.maxHP}</p>
+                <p>Lvl: {props.cat.level}</p>
+                <p>Power: {props.cat.power}</p>
+                {props.isTavern ? (
+                    <Button>Add to Party</Button>
+                ) 
+                : (
+                    <Button>Remove</Button>
+                )}
             </div>
+        </>
     )
 }
 
