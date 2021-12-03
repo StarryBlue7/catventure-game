8a1cf67e755c819583abef2e75ea2e32b8995540
Derek Banister <ptownderek@yahoo.com>
2021-12-03 14:12:48 -0800

    mapping out users cats into sidebar/asidecard

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import AsideCard from './gameUI/AsideCard'
+
+
+const fakeUser = {
+    username: "George",
+    password: "duck",
+    cats: [
+        {
+            name: "joe",
+            class: "Warr",
+            level: 1000,
+            power: 10,
+            experience: 1432,
+            maxHP: 1333,
+            currentHP: 1202,
+        }
+    ]
+
+}
 // Sidebar
 function Sidebar({ userData }) {
     return (
         <aside className="col-3">
-        </aside>
+            {fakeUser.cats.map((cat, i) => (
+                <AsideCard cat={cat} key={i} />
+            ))}
+
+
+        </aside >
     );
 }
 
--- client/src/components/gameUI/AsideCard.js
+++ client/src/components/gameUI/AsideCard.js
@@ -1,19 +1,18 @@
-
 import React from 'react';
 
-
-
-const AsideCard = () => {
+const AsideCard = (props) => {
 
 
     return (
         <section>
-            <h2>Your Party</h2>
-            {/* <div>
-            {user.map((cats, i) => (
-                
-            ))}
-            </div> */}
+            <h2>Your party</h2>
+            <p>{props.cat.name}</p>
+            <p>{props.cat.class}</p>
+            <p>{props.cat.level}</p>
+            <p>{props.cat.power}</p>
+            <p>{props.cat.experience}</p>
+            <p>{props.cat.maxHP}</p>
+            <p>{props.cat.currentHP}</p>
         </section>
     )
 }
