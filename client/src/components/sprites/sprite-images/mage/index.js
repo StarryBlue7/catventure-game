4e632c22d87c80278448ea4edebde461aa70ca77
Vince <starryblue7@gmail.com>
2021-12-04 10:45:04 -0800

    consolidate imports

---
+++ client/src/components/sprites/sprite-images/mage/index.js
@@ -0,0 +1,13 @@
+import die from './mage_die.png';
+import heal from './mage_heal.png';
+import idle from './mage_idle.png';
+import attack from './mage_attack.png';
+
+const MageSprites = { 
+    die, 
+    heal,
+    idle,
+    attack
+}
+
+export default MageSprites;
