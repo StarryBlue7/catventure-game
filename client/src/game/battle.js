12ec6e32682e62f71b14e950fed64cd12879efc2
Vince <starryblue7@gmail.com>
2021-12-05 14:41:53 -0800

    build turn order and positions data

--- client/src/game/battle.js
+++ client/src/game/battle.js
@@ -7,11 +7,12 @@ function randomEnemyCount() {
 
 // Build enemy
 class Enemy {
-    constructor(maxHP, power, img) {
-        this.maxHP = maxHP;
-        this.currentHP = maxHP;
+    constructor(maxHP, hpSpread, power, powerSpread, id, img) {
+        this.maxHP = randomGen(maxHP, hpSpread);
+        this.currentHP = randomGen(maxHP, powerSpread);
         this.power = power;
         this.img = img;
+        this.id = id
     }
 
     // Calculate damage done as a function of RNG & power stat
@@ -36,6 +37,48 @@ function partyTotals(party) {
     return { totalHP, totalPower};
 }
 
+// Generates enemies of random stats based on the count and party totals
 function generateEnemies(count, partyTotal) {
+    const hpSpread = 5;
+    const powerSpread = 3;
+    const baseHP = Math.ceil(partyTotal.totalHP / count);
+    const basePower = Math.ceil(partyTotal.totalPower / count);
 
+    let enemies = [];
+    for (let i = -1; i > - count - 1; i--) {
+        const randomImg = Math.floor() * 3;
+        enemies.push(new Enemy(baseHP, hpSpread, basePower, powerSpread, i, randomImg))
+    }
+    return enemies;
+}
+
+// party of cats [cat1, cat2, cat3]
+// enemies [enemy1, enemy2]
+
+// battlefield [cat1, cat2, cat3, enemy1, enemy2]
+
+// randomturns [0, 3, 2, 1, 4]
+
+// Generate a structure 
+function battlePositions(party, enemies) {
+    let positions = [];
+    party.forEach(cat => {
+        positions.push(cat._id);
+    });
+    enemies.forEach(enemy => {
+        positions.push(enemy.id);
+    })
+    return positions;
+}
+
+// Generate random turn order based on length of positions array
+function turnOrder(positions) {
+    let turnOrder = [];
+    while (turnOrder.length < positions.length) {
+        const order = Math.floor(Math.random() * positions.length);
+        if(turnOrder.indexOf(order) === -1) {
+            turnOrder.push(order);
+        }
+    }
+    return turnOrder;
 }
