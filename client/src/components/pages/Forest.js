6b02a3a45beba4a759ab6a9bdb5cee897c030b4d
Emily Dorgan <wmdorgan@gmail.com>
2021-12-05 12:18:01 -0800

    pseudocoded the pre-battle function

--- client/src/components/pages/Forest.js
+++ client/src/components/pages/Forest.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 
+// Pseudocode for the pseudocode
+
 // Make a class constructor for the enemies with the following object keys:
 // currentHP
 // maxHP
@@ -32,10 +34,23 @@ import { Button } from 'react-bootstrap';
 
 // an array keeps track of the turn order
 
-// Pseudocode
+// **Pseudocode**
 
+// preBattle function:
 
-// 
+//  1. call the preBattle function when user enters the forest area
+//  2. make a variable called numEnemies set equal to a random number between 1 and 4.
+//  4. make a variable called enemyBaseHP, set equal to the sum of the player's cats' HP, divide by number of enemies
+//  5. make a variable called enemyBasePower, set equal to the sum of the player's cats' power, divide by number of enemies
+//  9. make an empty array called turnOrder
+//  6. Initialize a for loop wih i=0
+//  7. make a new Enemy object using the enemyBaseHP, enemyBasePower, randomly selected image and ID=i
+//  8. push that Enemy object's ID to the turnOrder array
+//  9. Repeat step 7 while i < numEnemies
+// 10. for each cat in the user's cat array, push the cat's ID to the turnOrder array
+// 11. Shuffle the turn order array
+
+//
 
 function Forest() {
     return (
