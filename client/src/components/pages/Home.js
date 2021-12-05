efc13ccd852d27106f0c38a41f1d569cdfbad64d
Vince <starryblue7@gmail.com>
2021-12-04 17:57:29 -0800

    set up basic landing page contents

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,34 +1,25 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
+import { Row, Button } from 'react-bootstrap';
 
 import Sprites from '../sprites/Sprites';
-
-const catData = {
-    class: "Rogue",
-}
 
 function Home() {
     const [action, setAction] = useState('idle')
     return (
         <section>
-            <h2>
-                Home Sweet Home
-            </h2>
-            <div>
-                <Sprites job={catData.class} action={action} setAction={setAction} />
-            </div>
-            <div>
-                <Button onClick={() => setAction('idle')}>Idle</Button>
-                <Button onClick={() => setAction('attack')}>Attack</Button>
-                <Button onClick={() => setAction('special')}>Special</Button>
-                <Button onClick={() => setAction('die')}>Die</Button>
-                <Button onClick={() => setAction('damaged')}>Damaged</Button>
-            </div>
-            <div>
+            <Row className={"d-flex flex-row gap-10 justify-content-center"}>
+                <h2>Embark upon your very own Catventure!</h2>
+            </Row>
+            <Row className={"d-flex flex-row gap-10 justify-content-center"}>
+                <Sprites job={"Mage"} action={action} setAction={setAction} scale={.5} />
+                <Sprites job={"Warrior"} action={action} setAction={setAction} scale={.5} />
+                <Sprites job={"Rogue"} action={action} setAction={setAction} scale={.5} />
+            </Row>
+            <Row className={"d-flex flex-row gap-10 justify-content-center"}>
                 <Button as={Link} to="/village">Go on Adventure</Button>
                 <Button as={Link} to="/party">View Party Details</Button>
-            </div>
+            </Row>
         </section>
     )
 }
