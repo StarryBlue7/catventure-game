6d1dcccee302dded41bd4905cece0e971117ae50
Vince <starryblue7@gmail.com>
2021-12-05 15:15:40 -0800

    testing initial battle setup functionality

--- client/src/game/battle.js
+++ client/src/game/battle.js
@@ -1,4 +1,7 @@
-import randomGen from '../utils/RNG'
+// Randomizer
+function randomGen(baseValue, spread) {
+    return Math.abs(baseValue - spread + Math.floor(Math.random() * (spread * 2 + 1)))
+}
 
 // Randomly determine quantity of enemies
 function randomEnemyCount() {
@@ -9,17 +12,15 @@ function randomEnemyCount() {
 class Enemy {
     constructor(maxHP, hpSpread, power, powerSpread, id, img) {
         this.maxHP = randomGen(maxHP, hpSpread);
-        this.currentHP = randomGen(maxHP, powerSpread);
-        this.power = power;
+        this.currentHP = this.maxHP;
+        this.power = randomGen(power, powerSpread);
         this.img = img;
         this.id = id
     }
-
     // Calculate damage done as a function of RNG & power stat
     calcDamage() {
         return Math.ceil(Math.random * this.power);
     }
-
     // Prevent showing negative HPs
     showHP() {
         return this.currentHP >= 0 ? this.currentHP : 0;
@@ -46,18 +47,11 @@ function generateEnemies(count, partyTotal) {
 
     let enemies = [];
     for (let i = -1; i > - count - 1; i--) {
-        const randomImg = Math.floor() * 3;
+        const randomImg = Math.floor(Math.random() * 5);
         enemies.push(new Enemy(baseHP, hpSpread, basePower, powerSpread, i, randomImg))
     }
     return enemies;
 }
-
-// party of cats [cat1, cat2, cat3]
-// enemies [enemy1, enemy2]
-
-// battlefield [cat1, cat2, cat3, enemy1, enemy2]
-
-// randomturns [0, 3, 2, 1, 4]
 
 // Generate a structure 
 function battlePositions(party, enemies) {
@@ -67,7 +61,7 @@ function battlePositions(party, enemies) {
     });
     enemies.forEach(enemy => {
         positions.push(enemy.id);
-    })
+    });
     return positions;
 }
 
@@ -82,3 +76,20 @@ function turnOrder(positions) {
     }
     return turnOrder;
 }
+
+function battle(party) {
+    const enemies = generateEnemies(randomEnemyCount(), partyTotals(party));
+    console.log(enemies);
+    const positions = battlePositions(party, enemies);
+    console.log(positions);
+    const turns = turnOrder(positions);
+    console.log(turns);
+}
+
+const party = [
+    {_id: 789, name: 'Derek', class: 'Warrior', maxHP: 60, currentHP: 60, power: 15, level: 3, experience: 5},
+    {_id: 456, name: 'Emily', class: 'Rogue', maxHP: 40, currentHP: 30, power: 19, level: 4, experience: 2},
+    {_id: 123, name: 'Vince', class: 'Mage', maxHP: 30, currentHP: 25, power: 25, level: 3, experience: 1}
+]
+
+battle(party)
