e87fdfe22c0ae88f8faef38a276456a6002a2117
Emily Dorgan <wmdorgan@gmail.com>
2021-12-06 20:12:29 -0800

    cat village button placement now looks like an actual video game

--- client/src/components/pages/Village.js
+++ client/src/components/pages/Village.js
@@ -8,6 +8,21 @@ const styles = {
     villageHeader: {
         fontSize: '3em',
         margin: '20px'
+    },
+    tavernBtn: {
+        position: 'absolute',
+        bottom: '210px',
+        left: '250px'
+    },
+    forestBtn: {
+        position: 'absolute',
+        top: '100px',
+        right: '100px'
+    },
+    caveBtn: {
+        position: 'absolute',
+        bottom: '200px',
+        right: '100px'
     }
 }
 
@@ -18,9 +33,9 @@ function Village() {
             <Col className={"location px-0 d-flex flex-column align-items-center"} >
                 <img src={village} alt={"Village"} />
                 <h2 style={styles.villageHeader}>Cat Village</h2>
-                <Button className={"game-button"} as={Link} to="/tavern">Go to the Tavern</Button>
-                <Button className={"game-button"} as={Link} to="/forest">Go to Deadly Forest</Button>
-                <Button className={"game-button"} as={Link} to="/cave">Go to Dark Cave</Button>
+                <Button style={styles.tavernBtn} className={"game-button"} as={Link} to="/tavern">Go to the Tavern</Button>
+                <Button style={styles.forestBtn} className={"game-button"} as={Link} to="/forest">Go to Deadly Forest</Button>
+                <Button style={styles.caveBtn} className={"game-button"} as={Link} to="/cave">Go to Dark Cave</Button>
             </Col>
     )
 }
