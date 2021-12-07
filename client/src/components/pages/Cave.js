916b75f9fce6ff79fb7cb02dfa5812f4db0ce2cd
Emily Dorgan <wmdorgan@gmail.com>
2021-12-06 19:46:37 -0800

    styled the cave page

--- client/src/components/pages/Cave.js
+++ client/src/components/pages/Cave.js
@@ -7,12 +7,24 @@ import treasure from '../../images/treasure.gif'
 import cave from '../../images/cave.png';
 
 const styles = {
+    caveHeader: {
+        marginTop: '20px',
+        fontSize: '2.5em'
+    },
+    caveText: {
+        fontSize: '1.2em'
+    },
     treasureBtn: {
         backgroundColor: 'transparent',
         padding: 0,
         border: '0px',
         outline: 'none',
         boxShadow: 'none'
+    },
+    noTreasure: {
+        margin: '30px',
+        marginTop: '100px',
+        fontSize: '1.2em'
     }
 }
 
@@ -28,9 +40,9 @@ function Cave({ userData }) {
         const boostedCats = userData.cats;
 
         let randomCat = Math.floor(Math.random() * boostedCats.length);
-        let boostAmount = 15 //Math.ceil(Math.random() * 3)
+        let boostAmount = Math.ceil(Math.random() * 3)
 
-        let randomBoost = 3 //Math.floor(Math.random() * 4)
+        let randomBoost = Math.floor(Math.random() * 4)
 
         let randomStat;
 
@@ -78,7 +90,7 @@ function Cave({ userData }) {
     // Looks at the userdata and figures out if they are in a lockout or not
     // if they are, disable the button
     const isLockout = () => {
-        const yesterday = new Date(new Date().setHours(new Date().getHours() - 0))
+        const yesterday = new Date(new Date().setHours(new Date().getHours() - 20))
         const usersDay = new Date(userData.lastTreasure);
 
         if (usersDay > yesterday) {
@@ -91,9 +103,9 @@ function Cave({ userData }) {
     return (
             <Col className={"location px-0 d-flex flex-column align-items-center"} style={styles.page}>
                 <img src={cave} alt={"Cave"} style={styles.background} />
-                <h2>Dark Cave</h2>
-                <p>The party searched the depths of the dark cave to uncover ancient relics for increasing their feline abilities</p>
-                {isLockout() ? (<p>No treasures left to find. Come back later!</p>) : (
+                <h2 style={styles.caveHeader}>Dark Cave</h2>
+                <p style={styles.caveText}>The party searched the depths of the dark cave to uncover ancient relics for increasing their feline abilities</p>
+                {isLockout() ? (<p style={styles.noTreasure}>No treasures left to find. Come back later!</p>) : (
                     <>
                         <p>Look through the cave to find and click on the treasure!</p>
                         <Button
@@ -104,7 +116,7 @@ function Cave({ userData }) {
                         </Button>
                     </>)
                 }
-                <Button as={Link} to="/village">Back to the Village</Button>
+                <Button className={"game-button"} as={Link} to="/village">Back to the Village</Button>
                 <Modal show={showTreasure} onHide={() => setShowTreasure(false)}>
                     <Modal.Body closeButton>
                         {showTreasure}
